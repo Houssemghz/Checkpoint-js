537b528844ae222db61b48ab2320f9e20a16dae1
houssem ghazouani <ghz.houssem@gmail.com>
2021-05-19 18:31:55 +0200

    checkpoint-js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container classname="container">
+        <Row className="row">
+          <Col className="columns">
+            <Form>
+              <Form.Group>
+                <br />
+                <i className="title">Choose Track Music To Upload</i>
+                <br />
+                <br />
+                <Form.File id="exampleFormControlFile1" />
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+        <Row className="row">
+          <Col className="columns">
+            <Form>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="save me" />
+              </Form.Group>
+              <Button variant="primary" type="submit" className="buttom">
+                Submit
+              </Button>
+              <br />
+              <br />
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
